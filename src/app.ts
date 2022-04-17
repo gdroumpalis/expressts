bb602a86ba1050ae355712cf0b2afe0f45681e56
George Droumpalis <georgiosdroubalis@hotmail.gr>
2022-04-17 19:17:19 +0300

    di

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,32 @@
 import express,{Request,Response} from "express";
-import bodyParser from "body-parser";
 import { RegisterRoutes } from "../build/routes";
 import swaggerUi from "swagger-ui-express";
+import winston from "winston";
+import expressWinston from 'express-winston';
 
 export const app = express();
 
-// Use body parser to read sent json payloads
-app.use(
-    bodyParser.urlencoded({
-        extended: true,
-    })
-);
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use("/api/swagger/ui", swaggerUi.serve, async (_req: Request, res: Response) => {
     return res.send(
         swaggerUi.generateHTML(await import("../build/swagger.json"))
     );
 });
+app.use(expressWinston.logger({
+    transports: [
+        new winston.transports.Console()
+    ],
+    format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.json()
+    ),
+    meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+    msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+    expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+    colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+    ignoreRoute: function () { return false; } // optional: allows to skip some log messages based on request and/or response
+}));
 
 RegisterRoutes(app);
+
