ac3ca22abe6440c6369dfafb83ac86f2718232ef
George Droumpalis <georgiosdroubalis@hotmail.gr>
2022-04-17 15:33:22 +0300

    fix controller name , add simple debug script

--- src/Ping/PingController.ts
+++ src/Ping/PingController.ts
@@ -2,7 +2,7 @@ import {Controller, Get, Route,} from "tsoa";
 import {PingResponse} from "./Models/PingResponse";
 
 @Route("system")
-export class UsersController extends Controller {
+export class PingController extends Controller {
     @Get("ping")
     public async getUser(): Promise<PingResponse> {
         return {
