8d39fa20b3682531dd6f54e699489258f109d9f7
George Droumpalis <georgiosdroubalis@hotmail.gr>
2022-04-17 14:37:48 +0300

    ping controller

--- src/Ping/PingController.ts
+++ src/Ping/PingController.ts
@@ -1,9 +1,12 @@
 import {Controller, Get, Route,} from "tsoa";
+import {PingResponse} from "./Models/PingResponse";
 
 @Route("system")
 export class UsersController extends Controller {
     @Get("ping")
-    public async getUser() {
-        return "ping";
+    public async getUser(): Promise<PingResponse> {
+        return {
+            message: `hello`
+        } ;
     }
 }
